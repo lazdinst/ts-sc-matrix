ba6e5840001d0e6406e23510a24338574456c4c4
talis lazdins <lazdinst@gmail.com>
2023-11-09 00:46:35 -0500

    pull units and rolling working

---
+++ apps/server/src/models/Unit.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+export interface IUnit extends Document {
+  race: 'protoss' | 'zerg' | 'terran';
+  name: string;
+  mins: number;
+  gas: number;
+  type: string;
+}
+
+const unitSchema: Schema<IUnit> = new Schema<IUnit>({
+  race: {
+    type: String,
+    required: true,
+    enum: ['protoss', 'zerg', 'terran'],
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  mins: {
+    type: Number,
+    required: true,
+  },
+  gas: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+const Unit: Model<IUnit> = mongoose.model<IUnit>('Unit', unitSchema);
+
+export default Unit;
