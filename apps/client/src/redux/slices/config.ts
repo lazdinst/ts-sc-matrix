023eee80d8671ac456bab5098c819a38b0b6a8b1
talis lazdins <lazdinst@gmail.com>
2023-11-20 12:43:26 -0500

    env config

--- apps/client/src/redux/slices/config.ts
+++ apps/client/src/redux/slices/config.ts
@@ -2,4 +2,8 @@ const PROTOCOL = import.meta.env.VITE_REACT_APP_API_PROTOCOL || 'http';
 const HOST = import.meta.env.VITE_REACT_APP_API_HOST || 'localhost';
 const PORT = import.meta.env.VITE_REACT_APP_API_PORT || 10000;
 
-export const API_URL = `${PROTOCOL}://${HOST}:${PORT}`;
+let API_URL = `${PROTOCOL}://${HOST}}`;
+if (import.meta.env.VITE_REACT_APP_ENVIRONMENT === 'dev') {
+  API_URL = `${PROTOCOL}://${HOST}:${PORT}`;
+}
+export { API_URL };
