e996b05f625853fe4a6d3a3d3b5fa4c36e0f48c0
lazdinst <lazdinst@gmail.com>
2023-11-22 21:01:49 -0500

    fixes

--- apps/client/src/app/components/CharacterGrid/index.tsx
+++ apps/client/src/app/components/CharacterGrid/index.tsx
@@ -24,18 +24,54 @@ export const CharacterSquare = styled.div`
   justify-content: center;
   align-items: center;
 
-  img {
-    width: 152px;
-    height: 152px;
-    @media (max-width: 800px) {
-      width: 100%;
-    }
-  }
-
   h2 {
     text-align: center;
     @media (max-width: 800px) {
       font-size: 1rem;
     }
   }
+`;
+
+export const CharacterRaceImage = styled.img`
+  width: 152px;
+  height: 152px;
+  padding: 1rem 0rem;
+`;
+
+export const CharacterSquareImage = styled.img`
+  width: 152px;
+  height: 152px;
+`;
+
+export const getRaceColor = (race: string) => {
+  if (!race) return;
+  const raceColors: {
+    zerg: string;
+    terran: string;
+    protoss: string;
+  } = {
+    zerg: '#8400ff',
+    terran: '#ff0000',
+    protoss: '#0004ff',
+  };
+
+  const result = (raceColors as any)[race]; // Use type assertion
+  return result;
+};
+
+interface PlayerNameHeaderProps {
+  textShadow?: string;
+}
+
+export const PlayerNameHeader = styled.div<PlayerNameHeaderProps>`
+  text-align: center;
+  font-family: 'eurostile';
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: 500;
+  font-size: 2rem;
+  text-shadow: 0 0 1.25rem
+    ${(props) =>
+      props.textShadow ? props.textShadow : props.theme.colors.accentColor};
+  margin: 10px 0;
 `;
--- apps/client/src/app/containers/CharacterRoll/index.tsx
+++ apps/client/src/app/containers/CharacterRoll/index.tsx
@@ -4,6 +4,10 @@ import { RootState } from '../../../redux/store';
 import {
   CharacterGridContainer,
   CharacterSquare,
+  CharacterSquareImage,
+  CharacterRaceImage,
+  PlayerNameHeader,
+  getRaceColor,
 } from '../../components/CharacterGrid';
 import {
   Card,
@@ -30,59 +34,29 @@ import {
   getUnitImage,
 } from '../../utils';
 
-const raceColors: {
-  zerg: string;
-  terran: string;
-  protoss: string;
-} = {
-  zerg: '#8400ff',
-  terran: '#ff0000',
-  protoss: '#0004ff',
-};
-
-interface PlayerNameHeaderProps {
-  textShadow?: string;
-}
-
-const PlayerNameHeader = styled.div<PlayerNameHeaderProps>`
-  text-align: center;
-  font-family: 'eurostile';
-  text-transform: uppercase;
-  color: #fff;
-  font-weight: 500;
-  font-size: 2rem;
-  text-shadow: 0 0 1.25rem
-    ${(props) =>
-      props.textShadow ? props.textShadow : props.theme.colors.accentColor};
-  margin: 10px 0;
-`;
-
 const CharacterRoll: React.FC = () => {
   const players = useSelector((state: RootState) => state.roller.players);
 
-  const getRaceColor = (race: string) => {
-    if (!race) return;
-
-    const result = (raceColors as any)[race]; // Use type assertion
-    return result;
-  };
   return (
     <>
       {players.map((player) => {
         return (
           <CharacterGridContainer>
             <CharacterSquare>
-              <img
+              <CharacterRaceImage
                 src={getSymbolImageByRace(player.race).symbol}
                 alt={player.race}
               />
               <PlayerNameHeader textShadow={getRaceColor(player.race)}>
-                <h3>{player.name}</h3>
+                {player.name}
               </PlayerNameHeader>
             </CharacterSquare>
             {player.units.map((unit, index) => (
               <CharacterSquare key={unit._id}>
-                <img src={getUnitImage(unit.name)} alt={unit.name} />
+                <CharacterSquareImage
+                  src={getUnitImage(unit.name)}
+                  alt={unit.name}
+                />
                 <UnitWrapper id="unit-wrapper" key={`${unit.name}${unit._id}`}>
                   <UnitDetails>
                     <UnitName>
