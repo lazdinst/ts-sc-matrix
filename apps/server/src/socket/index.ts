a3bf0f74cbb495996ffef1c521e4edf59838d487
lazdinst <lazdinst@gmail.com>
2023-12-04 07:27:59 -0500

    disband party

--- apps/server/src/socket/index.ts
+++ apps/server/src/socket/index.ts
@@ -15,6 +15,38 @@ export const clientParties: ClientPartiesType = new Map<
   PlayerConnection[]
 >();
 
+const disbandPartyById = (socketId: string) => {
+  // Look at clientLobby and find the user with the socket id
+  // look at the client parties to see if that user is in a party
+  // if they are, disband the party
+  // if they are not, do nothing
+  console.log('Disbanding party for socket id', socketId);
+  console.log('Client parties', clientParties);
+  const disconnectedPlayer = clientLobby.get(socketId);
+  if (!disconnectedPlayer) {
+    return;
+  }
+  if (!disconnectedPlayer) {
+    return;
+  }
+
+  for (const [partyId, partyMembers] of clientParties.entries()) {
+    const found = partyMembers.some(
+      (player) => JSON.stringify(player) === JSON.stringify(disconnectedPlayer)
+    );
+
+    if (found) {
+      console.log('Found player in party', disconnectedPlayer);
+      console.log('Party ID', partyId);
+      console.log('cLient lobby', clientLobby);
+      // get clientLobby ids for each party member that have partyId
+      // clientParties.delete(partyId);
+      break; // Exit the loop after deleting the party
+    }
+  }
+  console.log('Client parties', clientParties);
+};
+
 export const initializeSocketIO = (server: any) => {
   io = new SocketIoServer(server, {
     cors: {
@@ -37,8 +69,10 @@ export const initializeSocketIO = (server: any) => {
 
     socket.on('disconnect', () => {
       console.log('Client disconnected:', socket.id);
-      clientLobby.delete(socket.id);
       // TODO: Clean up clientParties by disbanding any parties that the client was in
+      disbandPartyById(socket.id);
+
+      clientLobby.delete(socket.id);
       emitLobbyToAllClients(clientLobby);
     });
   });
@@ -55,6 +89,24 @@ export const emitLobbyToAllClients = (
   if (lobby.length > 0) {
     console.log('Emitting to all lobby', lobby);
     io.sockets.emit('client-lobby', lobby);
+  }
+};
+
+export const emitPartyToPartyMembers = (clientParties: ClientPartiesType) => {
+  const parties = Array.from(clientParties.entries()).map((client) => {
+    return client[1];
+  });
+  if (parties.length > 0) {
+    console.log('Emitting to all Parties', lobby);
+    io.sockets.emit('client-lobby', lobby);
+
+    console.log('[Server] accept-party-invite:', partyInvite);
+    const socketIds = [
+      getSocketIdByUserId(partyInvite.recipient.id, clientLobby),
+      getSocketIdByUserId(partyInvite.sender.id, clientLobby),
+    ];
+
+    io.to(socketIds).emit('accept-party-invite', party);
   }
 };
 
